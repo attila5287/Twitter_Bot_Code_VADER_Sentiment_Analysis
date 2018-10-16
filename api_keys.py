444005818e6195d4a4bd81ea10f0007e376f83f6
attila5287 <42498753+attila5287@users.noreply.github.com>
2018-10-16 03:19:21 -0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# Replace with your API keys
+consumer_key = 'D0wvphKgUtO53D9YEDdb2S3pE'
+consumer_secret = "nXNbC3OVb1YiE9wbZpcCcMv6ZHALlkDKNvRHmHQ9zTcsW2BS3L"
+access_token = "874542160011382784-r6gpZVjS4eGWiBIqyLep33mJnBsHOd9"
+access_token_secret = "ybAzoK7ZU8uWpsfUdVmsWj5W1Oo2VNj3TUbtBv7H0JbQN"
